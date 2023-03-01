487b1d62528d934af1e79a29f590c8ddab6ab3d0
zefirchan <littlelanfear@gmail.com>
2023-02-28 20:16:49 -0500

    added failed password attempts and reset password

--- src/App.js
+++ src/App.js
@@ -67,6 +67,39 @@ const router = createBrowserRouter([
   
 ]);
 
+///////////////////////////Login handler////////////////////////////////////////
+const [failedAttempts, setFailedAttempts] = useState(0);
+  const [currentForm, setCurrentForm] = useState('login');
+  const [passwordHistory, setPasswordHistory] = useState([]);
+
+  function handleLogin(event) {
+    event.preventDefault();
+    const password = event.target.password.value;
+    if (password === "correctPassword") {
+      // Login successful
+    } else {
+      // Login failed
+      setFailedAttempts(failedAttempts + 1);
+      if (failedAttempts === 2) {
+        alert("Your account has been suspended.");
+        event.target.password.disabled = true;
+      }
+    }
+  }
+
+  //////////////////Password Reset///////////////////////////////////////
+  function handleResetPassword(event) {
+    event.preventDefault();
+    const newPassword = event.target.newPassword.value;
+    if (passwordHistory.includes(newPassword)) {
+      alert("You cannot use an old password.");
+    } else {
+      setPasswordHistory([...passwordHistory, newPassword]);
+      alert("Password changed successfully.");
+    }
+  }
+
+  /////////////////////////Routing///////////////////////////////////
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
